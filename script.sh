b822432c1f99b73eb447ab7b0380bfca6ccdfb8c
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-09-13 21:16:16 +0200

    Creation du script pour telecharger les installateur

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check de systeme d'exploitation installer 
+
+. /etc/os-release 2>/dev/null || true
+case "${ID:-}:${VERSION_ID:-}" in
+  rhel:10*)   echo Telechargement du script pour RHEL10 ; 
+  wget -O- https://raw.githubusercontent.com/Arrera-linux/elk-installer/refs/heads/main/install-rhel.sh | bash ;;
+  debian:13*) echo Telechargement du script pour Debian13 ; 
+  wget -O- https://raw.githubusercontent.com/Arrera-linux/elk-installer/refs/heads/main/install-d13.sh | bash ;;
+  debian:12*) echo Telechargement du script pour Debian12 ; 
+  wget -O- https://raw.githubusercontent.com/Arrera-linux/elk-installer/refs/heads/main/install-d12.sh | bash ;;
+  *)          echo "Imposible de lancer le script sur cette distribution" ;;
+esac
